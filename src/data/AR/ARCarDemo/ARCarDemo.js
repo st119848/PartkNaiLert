956549a1e63f066101d58371c619eaab359916ea
Sumet Ma-in <mek@fazwaz.com>
2019-09-30 21:09:23 +0700

    feat(android): add new ar version lib

--- src/data/AR/ARCarDemo/ARCarDemo.js
+++ src/data/AR/ARCarDemo/ARCarDemo.js
@@ -1,10 +1,8 @@
 'use strict';
 
 import React, { Component } from 'react';
-
 import { StyleSheet } from 'react-native';
-
-import NLdata from './res/NLdata.json';
+import ARData from './res/ARData.json';
 
 import {
   ViroARScene,
@@ -20,83 +18,110 @@ import {
   ViroQuad,
   ViroText,
   ViroConstants,
-  ViroFlexView
+  ViroFlexView,
+  ViroARSceneNavigator
 } from 'react-viro';
 
 const createReactClass = require('create-react-class');
 
 const ARCarDemo = createReactClass({
 
+  allMarkers: ["1", "2", "3", "4", "5", "6", "7", "8", "17"],
+
   getInitialState() {
-    return {
+    const baseState =
+    {
       texture: 'white',
       playAnim: false,
       animateCar: false,
+      isShow: false
     }
+    const varyState = {}
+    this.allMarkers.forEach((marker) => {
+      varyState['isShow' + marker] = false;
+    })
+    return { ...baseState, ...varyState }
   },
 
   render: function () {
 
     return (
       <ViroARScene>
-        <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
-          <Viro3DObject
-            scale={[0, 0, 0]}
-            source={require('./res/3D/jar.obj')}
-            resources={[require('./res/3D/jar_mtl.mtl'),
-            ]}
-            type="OBJ"
-            materials={this.state.texture}
-            onClick={this._toggleButtons}
-            position={[0.05, 0, 0.05]}
-            rotation={[0, 0, -90]}
-            animation={{ name: "scaleCar", run: this.state.animateCar, }} />
-          <ViroFlexView
-            rotation={[-90, -90, 0]}
-            height={0.03}
-            width={0.05}
-            style={styles.card}
-          >
+        {this.allMarkers.map((marker) => (
+          <ViroARImageMarker target={marker} onAnchorFound={() => this._onAnchorFound(marker)} pauseUpdates={this.state.pauseUpdates}>
+            <Viro3DObject
+              scale={[0, 0, 0]}
+              source={require('./res/3D/jar_obj.obj')}
+              resources={[require('./res/3D/jar.mtl'),]}
+              type="OBJ"
+              materials={this.state.texture}
+              onClick={this._toggleButtons}
+              position={[0.05, 0, 0.05]}
+              rotation={[0, 0, -90]}
+              visible={this.state["isShow" + marker]}
+              animation={{ name: "scaleCar", run: this.state.animateCar, }} />
             <ViroFlexView
-              style={styles.cardWrapper}
+              rotation={[-90, -90, 0]}
+              height={0.03}
+              width={0.05}
+              style={styles.card}
+              visible={this.state["isShow" + marker]}
             >
-              <ViroText
-                textClipMode="None"
-                text={String(NLdata.value[0].detail)}
-                scale={[.015, .015, .015]}
-                position={[-0.05, 0, 0.05]}
-                style={styles.textStyle}
-              />
+              <ViroFlexView
+                style={styles.cardWrapper}
+              >
+                <ViroText
+                  textClipMode="None"
+                  text={String(ARData[marker-1].value[0].detail)}
+                  scale={[.015, .015, .015]}
+                  position={[-0.05, 0, 0.05]}
+                  style={styles.textStyle}
+                />
+              </ViroFlexView>
             </ViroFlexView>
-          </ViroFlexView>
-          <ViroSpotLight
-            innerAngle={5}
-            outerAngle={25}
-            direction={[0, -1, 0]}
-            position={[0, 5, 1]}
-            color="#ffffff"
-            castsShadow={true}
-            shadowMapSize={2048}
-            shadowNearZ={2}
-            shadowFarZ={7}
-            shadowOpacity={.7}
-          />
+            <ViroSpotLight
+              innerAngle={5}
+              outerAngle={25}
+              direction={[0, -1, 0]}
+              position={[0, 5, 1]}
+              color="#ffffff"
+              castsShadow={true}
+              shadowMapSize={2048}
+              shadowNearZ={2}
+              shadowFarZ={7}
+              shadowOpacity={.7}
+            />
 
-          <ViroQuad
-            rotation={[-90, 0, 0]}
-            position={[0, -0.001, 0]}
-            width={2.5} height={2.5}
-            arShadowReceiver={true}
-          />
+            <ViroQuad
+              rotation={[-90, 0, 0]}
+              position={[0, -0.001, 0]}
+              width={2.5} height={2.5}
+              arShadowReceiver={true}
+            />
 
-        </ViroARImageMarker>
+          </ViroARImageMarker>
+        ))}
+
       </ViroARScene>
     );
   },
-  _onAnchorFound() {
-    this.setState({
-      animateCar: true,
-    })
+  _onAnchorFound(marker) {
+
+    // Show only when isShow is all false 
+    let allNotShow = true;
+    console.log(marker);
+    this.allMarkers.forEach((marker) => {
+
+      if (this.state['isShow' + marker]) {
+        console.log("all not show false ")
+        allNotShow = false;
+      }
+    });
+    if (allNotShow) {
+      let stateForSet = { animateCar: true };
+      stateForSet["isShow" + marker] = true;
+      this.setState(stateForSet);
+    }
   },
 });
 
@@ -104,21 +129,65 @@ ViroMaterials.createMaterials({
   white: {
     shininess: 2.0,
     lightingModel: "PBR",
-    diffuseTexture: require('./res/Test2.jpg'),
   }
 });
 
 ViroARTrackingTargets.createTargets({
-  logo: {
-    source: require('./res/Test2.jpg'),
+  "1": {
+    source: require('./res/1.jpeg'),
     orientation: "Left",
     physicalWidth: 0.165 // real world width in meters
   },
-  // logo: {
-  //   source: require('./res/logo2.png'),
-  //   orientation: "Left",
-  //   physicalWidth: 0.165 // real world width in meters
-  // }
+  "2": {
+    source: require('./res/2.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "3": {
+    source: require('./res/3.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "4": {
+    source: require('./res/4.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "5": {
+    source: require('./res/5.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "6": {
+    source: require('./res/6.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "6": {
+    source: require('./res/9.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "7": {
+    source: require('./res/7.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "8": {
+    source: require('./res/8.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "17": {
+    source: require('./res/171.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  },
+  "17": {
+    source: require('./res/172.jpeg'),
+    orientation: "Left",
+    physicalWidth: 0.165 // real world width in meters
+  }
 });
 
 ViroAnimations.registerAnimations({
