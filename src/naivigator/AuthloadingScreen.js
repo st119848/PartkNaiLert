d903a7dbc495c7659d602be76e01c2bd02c14d2b
Sumet Ma-in <mek@fazwaz.com>
2019-10-28 22:04:22 +0700

    feat(app): add loading screen

--- src/naivigator/AuthloadingScreen.js
+++ src/naivigator/AuthloadingScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { ActivityIndicator, View } from 'react-native';
+import {
+    ActivityIndicator,
+    View,
+    Image,
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-// import SplashScreen from 'react-native-splash-screen'
 import BeaconScan from '../../tools/BeaconScan'
+import logo from './../assets/img/logo.png'
+import logoName from './../assets/img/logoName.png'
 
 import { connect } from 'react-redux'
 import { settingLanguage } from "../reducers/actions/setting";
@@ -12,22 +19,21 @@ import { settingLanguage } from "../reducers/actions/setting";
     super()
     this._LoadApp()
   }
-  componentDidMount(){
-    // SplashScreen.hide()
-  }
   _LoadApp = async () => {
       const selectedLanguage = await AsyncStorage.getItem('selectedLanguage');
       if(selectedLanguage && (selectedLanguage !== null)) {
           this.props.settingLanguage(selectedLanguage)
       }
-      this.props.navigation.navigate('Main')
+      // this.props.navigation.navigate('Main')
   };
 
   render() {
     return (
-      <View>
-        <ActivityIndicator />
-        <BeaconScan/>
+      <View style={styles.container}>
+          <Image source={logo} style={styles.logo}/>
+          <Image source={logoName} style={styles.logoName}/>
+          <ActivityIndicator />
+          <BeaconScan/>
       </View>
     )
   }
@@ -47,4 +53,25 @@ const mapStateToProps = (state) => {
 }
 export default connect(mapStateToProps, mapDispatchToProps)(AuthLoadingScreen)
 // export default AuthLoadingScreen
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(242, 218, 217)',
+    },
+    logo: {
+        resizeMode: 'contain',
+        width: width/100*40,
+        height: width/100*30,
+    },
+    logoName: {
+        resizeMode: 'contain',
+        width: width/100*60,
+        height: width/100*20,
+    }
+});
 
