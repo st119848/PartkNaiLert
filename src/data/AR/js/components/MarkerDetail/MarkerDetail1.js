c08ce7376c96fac261cb3f3cb5a698584b3bab6b
Sumet Ma-in <mek@fazwaz.com>
2020-01-21 23:40:34 +0700

    feat(AR): update ar images

--- src/data/AR/js/components/MarkerDetail/MarkerDetail1.js
+++ src/data/AR/js/components/MarkerDetail/MarkerDetail1.js
@@ -15,14 +15,43 @@ import { ScrollView } from 'react-native';
 
 export default class MarkerDetail1 extends Component {
 	render() {
-		const {marker} = this.props;
-		if (marker == 4) {
+		if (this.props.marker == 1) {
+			var sauce = require("../../../assets/1.jpeg");
+		} else if (this.props.marker == 2) {
+			var sauce = require("../../../assets/2.jpeg");
+		} else if (this.props.marker == 3) {
+			var sauce = require("../../../assets/3.jpeg");
+		} else if (this.props.marker == 4) {
 			var sauce = require("../../../assets/4.jpeg");
-		} else if (marker == 13) {
+		} else if (this.props.marker == 5) {
+			var sauce = require("../../../assets/5.jpeg");
+		} else if (this.props.marker == 6) {
+			var sauce = require("../../../assets/6.jpeg");
+		} else if (this.props.marker == 7) {
+			var sauce = require("../../../assets/7.jpeg");
+		} else if (this.props.marker == 8) {
+			var sauce = require("../../../assets/8.jpeg");
+		} else if (this.props.marker == 9) {
+			var sauce = require("../../../assets/9.jpeg");
+		} else if (this.props.marker == 10) {
+			var sauce = require("../../../assets/10.jpeg");
+		} else if (this.props.marker == 11) {
+			var sauce = require("../../../assets/11.jpeg");
+		} else if (this.props.marker == 12) {
+			var sauce = require("../../../assets/12.jpeg");
+		} else if (this.props.marker == 13) {
 			var sauce = require("../../../assets/13.jpeg");
-		} else if (marker == 16) {
+		} else if (this.props.marker == 14) {
+			var sauce = require("../../../assets/14.jpeg");
+		} else if (this.props.marker == 15) {
+			var sauce = require("../../../assets/15.jpeg");
+		} else if (this.props.marker == 16) {
 			var sauce = require("../../../assets/16.jpeg");
-		} else if (marker == 19) {
+		} else if (this.props.marker == 17) {
+			var sauce = require("../../../assets/17.jpeg");
+		} else if (this.props.marker == 18) {
+			var sauce = require("../../../assets/18.jpeg");
+		} else if (this.props.marker == 19) {
 			var sauce = require("../../../assets/19.jpeg");
 		}
 
--- src/data/AR/js/components/MarkerDetail/MarkerDetail2.js
+++ src/data/AR/js/components/MarkerDetail/MarkerDetail2.js
@@ -15,13 +15,36 @@ import { ScrollView } from 'react-native';
 
 export default class MarkerDetail2 extends Component {
 	render() {
-		const {marker} = this.props;
-		if (marker == 24) {
+		if (this.props.marker == 20) {
+			var sauce = require("../../../assets/20.jpeg");
+		} else if (this.props.marker == 21) {
+			var sauce = require("../../../assets/21.jpeg");
+		} else if (this.props.marker == 22) {
+			var sauce = require("../../../assets/22.jpeg");
+		} else if (this.props.marker == 23) {
+			var sauce = require("../../../assets/23.jpeg");
+		} else if (this.props.marker == 24) {
 			var sauce = require("../../../assets/24.jpeg");
-		} else if (marker == 31) {
+		} else if (this.props.marker == 25) {
+			var sauce = require("../../../assets/25.jpeg");
+		} else if (this.props.marker == 26) {
+			var sauce = require("../../../assets/26.jpeg");
+		} else if (this.props.marker == 27) {
+			var sauce = require("../../../assets/27.jpeg");
+		} else if (this.props.marker == 28) {
+			var sauce = require("../../../assets/28.jpeg");
+		} else if (this.props.marker == 29) {
+			var sauce = require("../../../assets/29.jpeg");
+		} else if (this.props.marker == 30) {
+			var sauce = require("../../../assets/30.jpeg");
+		} else if (this.props.marker == 31) {
 			var sauce = require("../../../assets/31.jpeg");
-		} else if (marker == 32) {
+		} else if (this.props.marker == 32) {
 			var sauce = require("../../../assets/32.jpeg");
+		} else if (this.props.marker == 33) {
+			var sauce = require("../../../assets/33.jpeg");
+		} else if (this.props.marker == 34) {
+			var sauce = require("../../../assets/34.jpeg");
 		}
 		return (
 			<Theme>
