8f1c260dd7d3b770177a5ad2937364feeb3311f7
Sumet Ma-in <mek@fazwaz.com>
2020-01-22 21:36:36 +0700

    fix(app): set lang default to en

--- src/helpers/translates.js
+++ src/helpers/translates.js
@@ -1,7 +1,7 @@
 import {get} from 'lodash';
 import TRANSLATES from './../config/translates'
 
-export const translate = (lang='us', key='', find='', replace='') => {
+export const translate = (lang='en', key='', find='', replace='') => {
     const translated = get(TRANSLATES, `${lang}.${key}`, '');
     return translated.replace(find, replace);
 };
